03b1f89c46da1f704b6ee6968ef6bede1398ee06
Nikolay Zenchenko <nik2704@yandex.ru>
2024-11-11 15:40:22 +0400

    comments removed

--- utils.h
+++ utils.h
@@ -25,7 +25,7 @@ inline std::optional<std::string> extract_ip(const std::string& input) {
         for (int i = 0; i < 4; ++i) {
             std::getline(ss, octet, '.');
             int octet_value = std::stoi(octet);
-            
+
             if (octet_value < 0 || octet_value > 255) {
                 return std::nullopt;
             }
@@ -35,21 +35,3 @@ inline std::optional<std::string> extract_ip(const std::string& input) {
 
     return std::nullopt;
 }
-
-// void filter_and_print(const ip_filter::IPAddressList& ip_list, const std::function<bool(const ip_filter::IPAddress&)>& predicate) {
-//     auto custom_filtered_ips = ip_list.filter(predicate);
-
-//     for (const auto& ip : custom_filtered_ips) {
-//         std::cout << ip << std::endl;
-//     }
-// }
-
-// std::vector<ip_filter::IPAddress> filter_first_and_second_byte(const std::multiset<ip_filter::IPAddress, ip_filter::ReverseIPCompare>& ip_addresses, int first_byte, int second_byte) {
-//     std::vector<ip_filter::IPAddress> result;
-//     for (const auto& addr : ip_addresses) {
-//         if (addr.get_octets()[0] == first_byte && addr.get_octets()[1] == second_byte) {
-//             result.push_back(addr);
-//         }
-//     }
-//     return result;
-// }
